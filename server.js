9a689606e3e8d196fa912b9175c846ffaf669cd5
Jhonbeltran <jbeltranleon@gmail.com>
2016-07-19 23:28:28 -0500

    creo archivo server.js En el archivo server.js voy a controlar un mini servidor web usando express

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-var express = require('express')//Importamos nuestro microframework para servidores web
+'use strict'
+
+const express = require('express')//Importamos nuestro microframework para servidores web
+const app = express()
+const port = process.env.PORT || 8080//definimos el puerto al que va a escuchar el servidor
+
+app.get('/', function (req, res) {
+	res.send('Dudeeee')
+})
+
+app.listen(port, function(err){
+	//Si hubo un error muestrelo en consola y termine la ejecucion
+	if(err) return console.log("Hubo un error"),process.exit(1)
+
+	console.log(`El servidor ha iniciado y está escuchando en el puerto: ${port}`)
+})
