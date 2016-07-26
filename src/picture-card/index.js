88dfc034e1bb02f3e06a51f33f4b7aa90b2da2f1
Jhonbeltran <jbeltranleon@gmail.com>
2016-07-26 16:19:30 -0500

    Utilizo yo-yo para llamar funciones desde la vista, hago un update a medida que van cambiando los datos, uso una funcion en sas para dar estilos y por fin hay corazones en mi platzigram

--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,23 +1,44 @@
 'use strict'
 const yo = require('yo-yo')
 
-module.exports = function(pic){
-	return yo`<div class="card">
-    <div class="card-image">
-      <img class="activator" src="${pic.url}">
-    </div>
-    <div class="card-content">
-      	<a href="/user/${pic.user.username}" class="card-title">
-			<img src="${pic.user.avatar}" class="avatar">
-			<span class="username">${pic.user.username}</span>
-		</a>
-		<small class="right time">Hace 1 día</small>
-		<p>
-			<a class="left" href="#">
-				<i class="fa fa-heart-o" aria-hidden="true"></i>
+module.exports = function pictureCard(pic){
+	let el;
+
+	function render(picture) {
+		return yo`<div class="card ${picture.liked ? 'liked' : ''}">
+	    <div class="card-image">
+	      <img class="activator" src="${picture.url}">
+	    </div>
+	    <div class="card-content">
+	      	<a href="/user/${picture.user.username}" class="card-title">
+				<img src="${picture.user.avatar}" class="avatar">
+				<span class="username">${picture.user.username}</span>
 			</a>
-			<span class="left">${pic.likes}</span>
-		</p>
-    </div>
-  </div>`
+			<small class="right time">Hace 1 día</small>
+			<p>
+				<a class="left" href="#" onclick=${like.bind(null, true)}>
+					<i class="fa fa-heart-o" aria-hidden="true"></i>
+				</a>
+				<a class="left" href="#" onclick=${like.bind(null, false)}>
+					<i class="fa fa-heart" aria-hidden="true"></i>
+				</a>
+				<span class="left">${picture.likes}</span>
+			</p>
+	    </div>
+	  </div>`
+	}
+	//usamos bind para evitar un bucle infinito
+  	function like(liked) {
+	  	pic.liked = liked
+	  	pic.likes += liked ? 1 : -1
+	  	let newEl = render(pic);
+	  	yo.update(el, newEl)
+	  	return false
+
+  	}
+
+  	
+
+  el = render(pic)
+  return el
 }
